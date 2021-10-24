a0fc8adc35d8ca4d230c01f32150294d19df11ca
bomnaly <bomnalyday@gmail.com>
2021-10-24 21:12:15 +0900

    Hello Java

---
+++ src/hello/hello.java
@@ -0,0 +1,17 @@
+/*
+ Date: 2021.10.24
+ Author: moonstal
+ Description: 첫번째 자바 프로그램
+ Version: 1.0
+ */
+
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+
+	}
+
+}
